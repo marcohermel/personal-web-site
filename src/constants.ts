2f60aba0fcbd8572cd7a9b2bba0f2dddb51bb4c9
Marco Hermel <marco.hermel@avenuecode.com>
2024-03-29 19:34:35 -0300

    configured tailwind css

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const MOBILE_LIMIT_WIDTH = 1024;
+
+export const COLOR = {
+    WHITE: '#FFFFFF',
+    PRIMARY: '#1C4C41',
+    SECONDARY: '#D9D9D9',
+    BLACK: '#000000'
+}
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+
+      white: '#FFF',
+      primary: '#1C4C41',
+      secondary: '#D9D9D9',
+      black: '#000',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
